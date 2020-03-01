34d7ea65a628b83f80bf83edad4285c4b4329638
KOGA Mitsuhiro <shiena.jp@gmail.com>
2020-03-01 23:11:05 +0900

    Add success button

---
+++ UnityAndroidToasty/Assets/Scripts/Toasty.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Toasty : MonoBehaviour
+{
+#if UNITY_ANDROID
+    private AndroidJavaClass _jc;
+
+    private void OnEnable()
+    {
+        _jc = new AndroidJavaClass("com.github.shiena.toastywrapper.ToastyWrapper");
+    }
+
+    private void OnDisable()
+    {
+        _jc?.Dispose();
+    }
+
+    public void Success(string message)
+    {
+        _jc?.CallStatic("success", message);
+    }
+#else
+    public void Success(string message)
+    {
+    }
+#endif
+}
